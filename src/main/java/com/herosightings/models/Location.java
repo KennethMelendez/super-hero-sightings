9bfbad277e1e6719cf18418c72058cff115a7550
KennethMelendez <kmlndz221@gmail.com.com>
2021-09-28 19:43:09 -0400

    Adding more getset to the Location object

--- src/main/java/com/herosightings/models/Location.java
+++ src/main/java/com/herosightings/models/Location.java
@@ -5,6 +5,21 @@ import lombok.Data;
 @Data
 public class Location {
 
+    String country;
+
+    String state;
+
+    String city;
+
+    String street;
+
+    String zip;
+
     String locationInfo;
+
+    String getAddress(){
+        return String.format("%s %s %s %s %s",
+                street, state, city, zip, country);
+    }
 
 }
